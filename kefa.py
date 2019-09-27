0bfe710e6b9fbce26a72d44284d4d2eababb6d9c
vanlongvl99 <nguyenvanlongt2@gmail.com>
2019-09-27 16:57:26 +0700

    kefa.py

--- kefa.py
+++ kefa.py
@@ -5,9 +5,20 @@ path = [-1]*(n+1)
 graph = [[] for i in range(n+1)]
 for i in range(n-1):
     first, second = map(int,input().split())
-    path[max(first, second)] = min(first, second) 
+    # path[max(first, second)] = min(first, second) 
     graph[first].append(second)
     graph[second].append(first)
+visited = [False for i in range(n + 1)]
+visited[1] = True
+queueQ = Queue()
+queueQ.put(1)
+while not queueQ.empty():
+    u = queueQ.get()
+    for i in graph[u]:
+        if visited[i] == False:
+            visited[i] = True
+            queueQ.put(i)
+            path[i] = u
 total = 0
 for i in range(2, n+1):
     if len(graph[i]) == 1: # các leaf vertices thì chỉ liên kết với đúng 1 đỉnh khác
